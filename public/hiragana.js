cb28bb40a6a9cfaedf801ba0ffe99268563a9a7b
ckstmznf <ckstmznf@naver.com>
2022-04-13 00:23:20 +0900

    data setting

---
+++ public/hiragana.js
@@ -0,0 +1,324 @@
+export default [
+{
+    'huragana': 'あ',
+    'pronunciation-en': 'a',
+    'pronunciation-ko': '아',
+    'column': 'a',
+    'row': 'a'
+},
+{
+    'huragana': 'い',
+    'pronunciation-en': 'ai',
+    'pronunciation-ko': '이',
+    'column': 'i',
+    'row': 'a'
+},
+{
+    'huragana': 'う',
+    'pronunciation-en': 'au',
+    'pronunciation-ko': '우',
+    'column': 'u',
+    'row': 'a'
+},
+{
+    'huragana': 'え',
+    'pronunciation-en': 'ae',
+    'pronunciation-ko': '에',
+    'column': 'e',
+    'row': 'a'
+},
+{
+    'huragana': 'お',
+    'pronunciation-en': 'ao',
+    'pronunciation-ko': '오',
+    'column': 'o',
+    'row': 'a'
+},
+{
+    'huragana': 'か',
+    'pronunciation-en': 'ka',
+    'pronunciation-ko': '카',
+    'column': 'a',
+    'row': 'ka'
+},
+{
+    'huragana': 'き',
+    'pronunciation-en': 'ki',
+    'pronunciation-ko': '키',
+    'column': 'i',
+    'row': 'ka'
+},
+{
+    'huragana': 'く',
+    'pronunciation-en': 'ku',
+    'pronunciation-ko': '쿠',
+    'column': 'u',
+    'row': 'ka'
+},
+{
+    'huragana': 'け',
+    'pronunciation-en': 'ke',
+    'pronunciation-ko': '케',
+    'column': 'e',
+    'row': 'ka'
+},
+{
+    'huragana': 'こ',
+    'pronunciation-en': 'ko',
+    'pronunciation-ko': '코',
+    'column': 'o',
+    'row': 'ka'
+},
+{
+    'huragana': 'さ',
+    'pronunciation-en': 'sa',
+    'pronunciation-ko': '사',
+    'column': 'a',
+    'row': 'sa'
+},
+{
+    'huragana': 'し',
+    'pronunciation-en': 'si',
+    'pronunciation-ko': '시',
+    'column': 'i',
+    'row': 'sa'
+},
+{
+    'huragana': 'す',
+    'pronunciation-en': 'su',
+    'pronunciation-ko': '스',
+    'column': 'u',
+    'row': 'sa'
+},
+{
+    'huragana': 'せ',
+    'pronunciation-en': 'se',
+    'pronunciation-ko': '세',
+    'column': 'e',
+    'row': 'sa'
+},
+{
+    'huragana': 'そ',
+    'pronunciation-en': 'so',
+    'pronunciation-ko': '소',
+    'column': 'o',
+    'row': 'sa'
+},
+{
+    'huragana': 'た',
+    'pronunciation-en': 'ta',
+    'pronunciation-ko': '타',
+    'column': 'a',
+    'row': 'ta'
+},
+{
+    'huragana': 'ち',
+    'pronunciation-en': 'ti',
+    'pronunciation-ko': '치',
+    'column': 'i',
+    'row': 'ta'
+},
+{
+    'huragana': 'つ',
+    'pronunciation-en': 'tu',
+    'pronunciation-ko': '츠',
+    'column': 'u',
+    'row': 'ta'
+},
+{
+    'huragana': 'て',
+    'pronunciation-en': 'te',
+    'pronunciation-ko': '테',
+    'column': 'e',
+    'row': 'ta'
+},
+{
+    'huragana': 'と',
+    'pronunciation-en': 'to',
+    'pronunciation-ko': '토',
+    'column': 'o',
+    'row': 'ta'
+},
+{
+    'huragana': 'な',
+    'pronunciation-en': 'na',
+    'pronunciation-ko': '나',
+    'column': 'a',
+    'row': 'na'
+},
+{
+    'huragana': 'に',
+    'pronunciation-en': 'ni',
+    'pronunciation-ko': '니',
+    'column': 'i',
+    'row': 'na'
+},
+{
+    'huragana': 'ぬ',
+    'pronunciation-en': 'nu',
+    'pronunciation-ko': '누',
+    'column': 'u',
+    'row': 'na'
+},
+{
+    'huragana': 'ね',
+    'pronunciation-en': 'ne',
+    'pronunciation-ko': '네',
+    'column': 'e',
+    'row': 'na'
+},
+{
+    'huragana': 'の',
+    'pronunciation-en': 'no',
+    'pronunciation-ko': '노',
+    'column': 'o',
+    'row': 'na'
+},
+{
+    'huragana': 'は',
+    'pronunciation-en': 'ha',
+    'pronunciation-ko': '하',
+    'column': 'a',
+    'row': 'ha'
+},
+{
+    'huragana': 'ひ',
+    'pronunciation-en': 'hi',
+    'pronunciation-ko': '히',
+    'column': 'i',
+    'row': 'ha'
+},
+{
+    'huragana': 'ふ',
+    'pronunciation-en': 'hu',
+    'pronunciation-ko': '후',
+    'column': 'u',
+    'row': 'ha'
+},
+{
+    'huragana': 'へ',
+    'pronunciation-en': 'he',
+    'pronunciation-ko': '헤',
+    'column': 'e',
+    'row': 'ha'
+},
+{
+    'huragana': 'ほ',
+    'pronunciation-en': 'ho',
+    'pronunciation-ko': '호',
+    'column': 'o',
+    'row': 'ha'
+},
+{
+    'huragana': 'ま',
+    'pronunciation-en': 'ma',
+    'pronunciation-ko': '마',
+    'column': 'a',
+    'row': 'ma'
+},
+{
+    'huragana': 'み',
+    'pronunciation-en': 'mi',
+    'pronunciation-ko': '미',
+    'column': 'i',
+    'row': 'ma'
+},
+{
+    'huragana': 'む',
+    'pronunciation-en': 'mu',
+    'pronunciation-ko': '무',
+    'column': 'u',
+    'row': 'ma'
+},
+{
+    'huragana': 'め',
+    'pronunciation-en': 'me',
+    'pronunciation-ko': '메',
+    'column': 'e',
+    'row': 'ma'
+},
+{
+    'huragana': 'も',
+    'pronunciation-en': 'mo',
+    'pronunciation-ko': '모',
+    'column': 'o',
+    'row': 'ma'
+},
+{
+    'huragana': 'や',
+    'pronunciation-en': 'ya',
+    'pronunciation-ko': '야',
+    'column': 'a',
+    'row': 'ya'
+},
+{
+    'huragana': 'ゆ',
+    'pronunciation-en': 'yu',
+    'pronunciation-ko': '유',
+    'column': 'u',
+    'row': 'ya'
+},
+{
+    'huragana': 'よ',
+    'pronunciation-en': 'yo',
+    'pronunciation-ko': '요',
+    'column': 'o',
+    'row': 'ya'
+},
+{
+    'huragana': 'ら',
+    'pronunciation-en': 'ra',
+    'pronunciation-ko': '라',
+    'column': 'a',
+    'row': 'ra'
+},
+{
+    'huragana': 'り',
+    'pronunciation-en': 'ri',
+    'pronunciation-ko': '리',
+    'column': 'i',
+    'row': 'ra'
+},
+{
+    'huragana': 'る',
+    'pronunciation-en': 'ru',
+    'pronunciation-ko': '루',
+    'column': 'u',
+    'row': 'ra'
+},
+{
+    'huragana': 'れ',
+    'pronunciation-en': 're',
+    'pronunciation-ko': '레',
+    'column': 'e',
+    'row': 'ra'
+},
+{
+    'huragana': 'ろ',
+    'pronunciation-en': 'ro',
+    'pronunciation-ko': '로',
+    'column': 'o',
+    'row': 'ra'
+},
+{
+    'huragana': 'わ',
+    'pronunciation-en': 'wa',
+    'pronunciation-ko': '와',
+    'column': 'a',
+    'row': 'wa'
+},
+{
+    'huragana': 'を',
+    'pronunciation-en': 'o',
+    'pronunciation-ko': '오',
+    'column': 'o',
+    'row': 'wa'
+},
+{
+    'huragana': 'ん',
+    'pronunciation-en': 'enn',
+    'pronunciation-ko': '응',
+    'column': null,
+    'row': null
+}
+]
