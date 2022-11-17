7f64cbdeba4bcb6e90a424a2927ab41966dc8f16
Heechan <ckstmznf@naver.com>
2022-11-17 17:16:06 +0900

    Firebase init

---
+++ src/assets/firebase.js
@@ -0,0 +1,8 @@
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import firebaseConfig from "@/firebaseConfig";
+
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
+export { analytics }
---
+++ src/firebaseConfig.js
@@ -0,0 +1,9 @@
+export default  {
+    apiKey: "AIzaSyAemRGpTKzdnsFq3oJKG4m22JrK73j6Wc0",
+    authDomain: "hiragana-df219.firebaseapp.com",
+    projectId: "hiragana-df219",
+    storageBucket: "hiragana-df219.appspot.com",
+    messagingSenderId: "606607891564",
+    appId: "1:606607891564:web:6bef6ce15fa95e4f3c0c49",
+    measurementId: "G-X4QX2C0FL6"
+}
