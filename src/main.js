ee9a8b20134f30c2e8e26b0a721f1fc89673acbd
Heechan <ckstmznf@naver.com>
2022-11-20 01:08:58 +0900

    Firebase analytics

--- src/main.js
+++ src/main.js
@@ -1,7 +1,12 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-
 import router from './router/router.js'
+import { analytics } from "@/assets/firebase";
+import { logEvent } from "firebase/analytics";
+
+logEvent(analytics, "notification_received")
+
+
 
 createApp(App)
     .use(router)
